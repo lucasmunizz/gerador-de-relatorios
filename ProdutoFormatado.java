af0ed26dc5e1c94ae62fce9c2730a3713310f9a4
Lucas Muniz <lucaslima1048@gmail.com>
2023-07-20 16:58:02 -0300

    Create ProdutoFormatado

---
+++ ProdutoFormatado.java
@@ -0,0 +1,50 @@
+public class ProdutoFormatado implements Produto {
+    private Produto produto;
+    private Formatacao formatacao;
+
+    public ProdutoFormatado(Produto produto, Formatacao formatacao) {
+        this.produto = produto;
+        this.formatacao = formatacao;
+    }
+
+    @Override
+    public String formataParaImpressao() {
+        String textoFormatado = formatacao.aplicarFormatacao(produto.formataParaImpressao());
+        return textoFormatado;
+    }
+
+    @Override
+    public void setQtdEstoque(int qtdEstoque) {
+        produto.setQtdEstoque(qtdEstoque);
+    }
+
+    @Override
+    public void setPreco(double preco) {
+        produto.setPreco(preco);
+    }
+
+    @Override
+    public int getId() {
+        return produto.getId();
+    }
+
+    @Override
+    public String getDescricao() {
+        return produto.getDescricao();
+    }
+
+    @Override
+    public String getCategoria() {
+        return produto.getCategoria();
+    }
+
+    @Override
+    public int getQtdEstoque() {
+        return produto.getQtdEstoque();
+    }
+
+    @Override
+    public double getPreco() {
+        return produto.getPreco();
+    }
+}
