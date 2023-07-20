65ebffb6cf2f0f24d2a5f582bc3446e3b17fc49d
Lucas Muniz <lucaslima1048@gmail.com>
2023-07-20 11:52:52 -0300

    Implements a filter by description method

--- FiltragemDescricaoString.java
+++ FiltragemDescricaoString.java
@@ -1,20 +1,18 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class FiltragemPrecoIntervalo implements FiltroStrategy{
+public class FiltragemDescricaoString implements FiltroStrategy{
 
     @Override
     public List<Produto> filtra(List<Produto> produtos, String argumento) {
         List<Produto> produtosFiltrados = new ArrayList<>();
-        double precoMin = Double.parseDouble(argumento.split(",")[0]);
-        double precoMax = Double.parseDouble(argumento.split(",")[1]);
         for (Produto produto : produtos) {
-            double preco = produto.getPreco();
-            if (preco >= precoMin && preco <= precoMax) {
+            String descricao = produto.getDescricao();
+            if (descricao.toLowerCase().contains(argumento.toLowerCase())) {
                 produtosFiltrados.add(produto);
             }
         }
         return produtosFiltrados;
     }
-    
+
 }
